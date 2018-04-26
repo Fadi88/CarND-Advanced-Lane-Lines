48eab17798b6555885a5fa129698372d80acced7
Fadi Sedik <fady88@gmail.com>
2018-04-26 04:27:21 +0200

    fixed review findings

--- code/process_image.py
+++ code/process_image.py
@@ -233,21 +233,21 @@ def handle_undistored_image(img):
 	cv2.imwrite( img_file.replace("test_images/" , "output_images/lm_unwarped_") , lane_mask_unwarped)
 	cv2.imwrite( img_file.replace("test_images/" , "output_images/warped_") , warped)
 
-	quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
+	#quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
 	# For each y position generate random x position within +/-50 pix
 	# of the line base position in each case (x=200 for left, and x=900 for right)
-	leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) for y in ploty])
-	rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) for y in ploty])
+	#leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) for y in ploty])
+	#rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) for y in ploty])
 
-	leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
-	rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+	#leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+	#rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
 
 
 	# Fit a second order polynomial to pixel positions in each fake lane line
-	left_fit = np.polyfit(ploty, leftx, 2)
-	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-	right_fit = np.polyfit(ploty, rightx, 2)
-	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+	#left_fit = np.polyfit(ploty, leftx, 2)
+	#left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+	#right_fit = np.polyfit(ploty, rightx, 2)
+	#right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
 	ym_per_pix = 30/720 # meters per pixel in y dimension
 	xm_per_pix = 3.7/700 # meters per pixel in x dimension
@@ -255,8 +255,8 @@ def handle_undistored_image(img):
 	y_eval = np.max(ploty)
 
 	# Fit new polynomials to x,y in world space
-	left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
-	right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+	left_fit_cr = np.polyfit(ploty*ym_per_pix, left_fitx*xm_per_pix, 2)
+	right_fit_cr = np.polyfit(ploty*ym_per_pix, right_fitx*xm_per_pix, 2)
 	# Calculate the new radii of curvature
 	left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
 	right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
